54539e79a85acd7229f8f30d5287232fbbd76f31
Adrian <adrian@cosmicweb.io>
2024-10-03 22:39:49 +0200

    workspace invitation

---
+++ app/Mail/WorkspaceInvitation.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WorkspaceInvitation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $workspaceName;
+    public $userEmail;
+    public $link;
+
+    public function __construct(string $workspaceName, string $userEmail, string $link)
+    {
+        $this->workspaceName = $workspaceName;
+        $this->userEmail = $userEmail;
+        $this->link = $link;
+    }
+
+    public function build()
+    {
+        return $this
+            ->subject("You're invited to join the workspace: {$this->workspaceName}")
+            ->view('emails.workspace_invitation'); 
+    }
+}
