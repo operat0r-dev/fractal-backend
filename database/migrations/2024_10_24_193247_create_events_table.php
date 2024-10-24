4db290f248557022fcebfe0acc028b87841e85ef
Adrian Peczyński <kappa123.wow@gmail.com>
2024-10-24 21:41:31 +0200

    small fix

--- database/migrations/2024_10_24_193247_create_events_table.php
+++ database/migrations/2024_10_24_193247_create_events_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
-              $table->foreignId('event_type_id')->constrained('event_types')->onDelete('cascade');
+            $table->foreignId('event_type_id')->constrained('event_types')->onDelete('cascade');
             $table->foreignId('task_id')->constrained('tasks')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
