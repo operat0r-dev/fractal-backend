1cbac4b43003119ed4c1ecca271807ffd323c6ec
Adrian Peczyński <kappa123.wow@gmail.com>
2024-10-24 20:53:15 +0200

    add seeder

---
+++ database/seeders/EventTypeSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EventType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EventTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $eventTypes = [
+            ['title' => 'Label Assigned'],
+            ['title' => 'Label Unassigned'],
+            ['title' => 'User Assigned'],
+            ['title' => 'User Unassigned'],
+            ['title' => 'Moved'],
+            ['title' => 'Description Changed'],
+            ['title' => 'Title Changed'],
+        ];
+
+        foreach ($eventTypes as $eventType) {
+            EventType::create($eventType);
+        }
+    }
+}
